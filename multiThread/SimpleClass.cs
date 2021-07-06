6b9bb4ebc0638a731942847e87b334974a9dd7e0
Eduardo CFN <eduardo@verion.ind.br>
2021-07-06 16:01:46 -0300

    Add project files.

---
+++ multiThread/SimpleClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiThread
+{
+    class SimpleClass
+    {
+
+        public int numeroQualquer { get; set; }
+        public string stringQualquer { get; set; }
+        public float floatQualquer { get; set; }
+        public double doubleQualquer { get; set; }
+        public char charQualquer { get; set; }
+    }
+}
