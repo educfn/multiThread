a2588c81a14407f8c94bc7c664bb44541809583f
Eduardo CFN <eduardo@verion.ind.br>
2021-07-08 15:03:55 -0300

    *Aplicação funcionando normalmente criando 3 task´s apartir da task do método 'mainASync()'. *A Aplicação consegue processar mais de uma classe 'SimpleClass' que estaram armazenadas na 'lista' dentro da classe estática 'Semaforo'.

--- multiThread/Program.cs
+++ multiThread/Program.cs
@@ -9,19 +9,7 @@ namespace multiThread
     {
         static void Main(string[] args)
         {
-            SimpleClass classeSimples = new SimpleClass();
-
-            coletarValoresDoUsuario(ref classeSimples);
-            
-
-            /*
-            test(ref classeSimples);
-            escreverClasseNoConsole(classeSimples);
-
-            criarTextoJSON(classeSimples);
-            criarTextoNomeVariavel(classeSimples);
-            printarJSON_No_Console(classeSimples);*/
-
+            mainASync().GetAwaiter().GetResult();
         }
 
         #region coleta_valores_do_usuario
@@ -47,62 +35,128 @@ namespace multiThread
         #endregion coleta_valores_do_usuario
 
         #region metodos async
-        static async Task criarTextoJSON(SimpleClass simple)
+
+        static async Task mainASync()
         {
-            string json = Newtonsoft.Json.JsonConvert.SerializeObject(simple),
-                //TODO: Qualidade: Perguntar ao usuario aonde quer salvar e o nome do arquivo. Usar função que mostra o caminho para salvar.
-                   caminho = @"C:\Users\Eduardo CFN\Desktop\",
-                   nomeDoArquivo = "json.txt";
-                //TODO: Qualidade: Criar um 'loop' para ler a string 'json' e colocar o simbolo '\n' após toda virgula na string.
-            try
+            //TODO: Qualidade: Perguntar ao usuario aonde quer salvar e o nome do arquivo. Usar função que mostra o caminho para salvar.
+            const string localParaSalvarArquivos = "C:\\Users\\Eduardo CFN\\Desktop\\Pasta_de_Textos\\";
+            bool primeiraVez = true;
+            int quantidadeDeClassesParaProcessar = 0;
+
+            do
             {
-                File.WriteAllText(caminho + nomeDoArquivo, json);              
-            }
-            catch (Exception) { }
+                SimpleClass simplesClasse = null;
+                simplesClasse = Semaforo.removeItem();
+
+                
+                criarTextoJSON(simplesClasse, localParaSalvarArquivos, quantidadeDeClassesParaProcessar);
+                criarTextoNomeVariavel(simplesClasse, localParaSalvarArquivos, quantidadeDeClassesParaProcessar);
+                printarJSON_No_Console(simplesClasse);
+
+                quantidadeDeClassesParaProcessar--;
+
+                if (primeiraVez == true && Semaforo.Count() == 0)
+                {
+                    test_criarEAdicionarListaSimpleClass(1);
+                    primeiraVez = false;
+                    quantidadeDeClassesParaProcessar = Semaforo.Count();
+                } 
+
+            } while(Semaforo.Count() > 0);
 
             await Task.Delay(1);
-
-            
         }
 
-        static async Task criarTextoNomeVariavel(SimpleClass simple)
+        static async Task criarTextoJSON(SimpleClass simple, string ondeSalvar, int quantidadeDeArquivos = 0)
         {
-            string texto = "",
-                   caminho = @"C:\Users\Eduardo CFN\Desktop\",
-                   //TODO: Qualidade: Perguntar ao usuario aonde quer salvar. Usar função que mostra o caminho para salvar.
-                   nomeDoArquivo = "NomeVariavel_VariavelNome.txt";
-
-            texto = "InteiroQualquer:" + simple.InteiroQualquer + "\n"+
-                    "FloatQualquer:" + simple.FloatQualquer + "\n" +
-                    "DoubleQualquer:" + simple.DoubleQualquer + "\n" +
-                    "CharQualquer:" + simple.CharQualquer + "\n" +
-                    "StringQualquer:" + simple.StringQualquer + "\n";
-
-            try
+            do
             {
-                File.WriteAllText(caminho + nomeDoArquivo, texto);
-            }
-            catch (Exception) { }
+                if (simple != null)
+                {
+                    string temp_json = Newtonsoft.Json.JsonConvert.SerializeObject(simple),
+                           nomeDoArquivo = "json.txt";
+                    if (quantidadeDeArquivos > 0)
+                    {
+                        nomeDoArquivo = "json" + quantidadeDeArquivos + ".txt";
+                        quantidadeDeArquivos--;
+                    }
+
+                    string json = "";
+                    foreach (var caracter in temp_json)
+                    {
+                        if (caracter == ',') json += caracter + "\n";
+                        else json += caracter;
+                    }
+
+                    try
+                    {
+                        File.WriteAllText(ondeSalvar + nomeDoArquivo, json);
+                    }
+                    catch (Exception) { }
+
+                }
+
+            } while (quantidadeDeArquivos > 0);
+
+            await Task.Delay(1);           
+        }
+
+        static async Task criarTextoNomeVariavel(SimpleClass simple, string ondeSalvar, int quantidadeDeArquivos = 0)
+        {
+
+            do
+            {
+                if (simple != null)
+                {
+                    string texto = "",
+                           //TODO: Qualidade: Perguntar ao usuario aonde quer salvar. Usar função que mostra o caminho para salvar.
+                           nomeDoArquivo = "NomeVariavel_VariavelNome.txt";
+                    if (quantidadeDeArquivos > 0)
+                    {
+                        nomeDoArquivo = "NomeVariavel_VariavelNome"+ quantidadeDeArquivos + ".txt";
+                        quantidadeDeArquivos--;
+                    }
+
+                    texto = "InteiroQualquer:" + simple.InteiroQualquer + "\n" +
+                            "FloatQualquer:" + simple.FloatQualquer + "\n" +
+                            "DoubleQualquer:" + simple.DoubleQualquer + "\n" +
+                            "CharQualquer:" + simple.CharQualquer + "\n" +
+                            "StringQualquer:" + simple.StringQualquer + "\n";
+
+                    try
+                    {
+                        File.WriteAllText(ondeSalvar + nomeDoArquivo, texto);
+                    }
+                    catch (Exception) { }
+                }
+            } while (quantidadeDeArquivos > 0);
 
             await Task.Delay(1);
         }
 
         static async Task printarJSON_No_Console(SimpleClass simple)
         {
-            string json = Newtonsoft.Json.JsonConvert.SerializeObject(simple);
-
-            foreach (var caracter in json)
+            if (simple != null)
             {
-                if (caracter == ',') Console.Write("\n");
-                else Console.Write(caracter);
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(simple);
+
+                foreach (var caracter in json)
+                {
+                    if (caracter == ',') Console.Write("\n");
+                    else Console.Write(caracter);
+                }
+
+                Console.Write("\n\n\n");
             }
+
+            await Task.Delay(1);
         }
 
         #endregion metodos async
 
         #region metodos_de_teste
         //TODO: Criar metodo 'test', o mesmo ira preencher, com valores aleatórios, uma instancia da classe 'SimpleClass' que será recebida por referencia.
-        public static void test(ref SimpleClass simple)
+        public static void test_PreencherSimpleClass(ref SimpleClass simple)
         {
             if (simple == null)
             {
@@ -136,7 +190,7 @@ namespace multiThread
 
     }
 
-        public static void escreverClasseNoConsole(SimpleClass simple) 
+        public static void test_escreverClasseNoConsole(SimpleClass simple) 
         {
             if (simple == null)
             {
@@ -150,6 +204,23 @@ namespace multiThread
             Console.WriteLine("string qualquer: " + simple.StringQualquer);
             //Criando um expaço para os proximos output´s.
             Console.Write("\n\n");
+        }
+
+        public static void test_criarEAdicionarListaSimpleClass(int tamanhoDaLista)
+        {
+
+            if (tamanhoDaLista > 0)
+            {
+                for (int i = 0; i < tamanhoDaLista; i++)
+                {
+                    SimpleClass simple = new SimpleClass();
+                    test_PreencherSimpleClass(ref simple);
+                    Semaforo.addItem(simple);
+                }
+
+            }
+            else throw new Exception();
+
         }
 
         #endregion metodos_de_teste
--- multiThread/Semaforo.cs
+++ multiThread/Semaforo.cs
@@ -9,23 +9,42 @@ namespace multiThread
     public static class Semaforo
     {
         private static List<SimpleClass> Lista = new List<SimpleClass>();
+        private static bool usando_addItem = false;
+        private static bool usando_removeItem = false;
 
         public static void addItem(SimpleClass simples) 
         {
-            Lista.Add(simples);
+            if (usando_addItem == false)
+            {
+                    usando_addItem = true;
+                    Lista.Add(simples);
+                    usando_addItem = false;            
+            }
         }
 
-        public static SimpleClass removeItem(SimpleClass simples)
+        public static SimpleClass removeItem()
         {
-            SimpleClass tmp = Lista[0];
-            Lista.Remove(tmp);
-            return tmp;
+            if (usando_removeItem == false)
+            {
+                if (Lista.Count() > 0)
+                {
+                    usando_removeItem = true;
+                    SimpleClass tmp = Lista[0];
+                    Lista.Remove(tmp);
+                    usando_removeItem = false;
+                    return tmp;
+                }
+                else return null; 
+            }
+            else return null;
+
         }
 
         public static int Count()
         {
             return Lista.Count;
         }
+
     }
     
 }
